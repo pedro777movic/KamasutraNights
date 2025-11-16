81a298612c65d816c4c884d9b5f9f04447fca3fe
Semente Cosmic <sementecosmic@gmail.com>
2025-11-16 01:51:26 +0000

    Adapte a LP para a fonte Belezza.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,8 +10,8 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        body: ['Poppins', 'sans-serif'],
-        headline: ['Poppins', 'sans-serif'],
+        body: ['Belezza', 'sans-serif'],
+        headline: ['Belezza', 'sans-serif'],
         code: ['monospace'],
       },
       colors: {
